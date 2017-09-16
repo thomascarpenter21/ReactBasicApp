921d1748db9b453161727241ed5a8a91ece79b97
Thomas Carpenter <thomascarpenter@Thomass-MacBook-Pro.local>
2017-09-15 23:13:12 -0700

    again tried to revise onclick function

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
+const VideoList = props => {
 	const videoItems = props.videos.map(video => {
 		return (
 			<VideoListItem 
